c83ce164595a02d6a2987c69b3e50fb0fadb19bc
Ryan Nelson <ryannelson881995@gmail.com>
2018-10-11 17:47:21 -0500

    checking pull request

--- javascript/helpers/events.js
+++ javascript/helpers/events.js
@@ -3,14 +3,7 @@ const hoverButtonEvent = (buttonId) => {
     const button = document.getElementById(buttonId);
     button.addEventListener("hover", () => {
         
-    })
-}
-
-const clearButtonfunction = () => {
-    const clearButton = document.getElementById(clearAll-button);
-    clearButton.addEventListener("click", (e) => {
-
-    })
+    });
 };
 
 empty();
