616509f197cd2f4de2ff2f39c271e4c4a215baff
Mary Rys <rys.mary@gmail.com>
2018-10-10 20:24:42 -0500

    wip

--- javascript/helpers/events.js
+++ javascript/helpers/events.js
@@ -5,3 +5,12 @@ const hoverButtonEvent = (buttonId) => {
         
     })
 }
+
+const largeFontEvent = () => {
+    const checkBox = document.getElementById('customCheck2');
+    checkBox.addEventListener('click', () => {
+        document.getElementById('theSquare').style.fontSize = "40px";
+    });
+}
+
+export {largeFontEvent};
--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,9 @@
 import {getChatData} from './data/chatData.js';
+import {largeFontEvent} from './helpers/events.js'
 
 const initializeApp = () => {
     getChatData();
+    largeFontEvent();
 };
 
 initializeApp();
